2f7f082b6d270c4827869d28b3860151d91dccf1
rodrigo luiz <rodrigoluizandradedeoliveira@gmail.com>
2021-09-01 19:06:23 -0300

    Feito

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ function App() {
   const test = {
     child1: "Rodrigo",
     child2: "Julio",
+    child3: "Astrogilda",
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <Children children1={test.child1} />
+        <Children children1={test.child2} />
+        <Children children1={test.child3} />
       </header>
     </div>
   );
